ae5ee3bd5d5b1057310101a0ec2e9c7fbb180b1d
Homin Lee <ff4500@gmail.com>
2025-05-03 02:11:15 +0900

    clean up

--- rag.go
+++ rag.go
@@ -38,33 +38,29 @@ func NewLottoRAGAI(
 ) (*LottoRAGAI, error) {
 	ctx := context.Background()
 
-	// Initialize a Genkit instance.
-	g, err := genkit.Init(ctx,
-		// Install the Google AI plugin which provides Gemini models.
-		genkit.WithPlugins(
-			&googlegenai.GoogleAI{},
-			// &googlegenai.VertexAI{},
-		),
-		// Set the default model to use for generate calls.
-		genkit.WithDefaultModel("googleai/gemini-2.0-flash"),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize Genkit: %w", err)
-	}
-
 	vstore, err := NewWeaviateOllamaVectorStore()
 	if err != nil {
 		return nil, fmt.Errorf("failed to define indexer and retriever: %w", err)
 	}
 	log.Println(vstore.String(), "initialized")
 
-	ret := &LottoRAGAI{}
+	g, err := genkit.Init(ctx,
+		genkit.WithPlugins(
+			&googlegenai.GoogleAI{},
+		),
+		genkit.WithDefaultModel("googleai/gemini-2.0-flash"),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize Genkit: %w", err)
+	}
+
+	rag := &LottoRAGAI{}
 
 	indexFlow := genkit.DefineFlow(
 		g, "indexWinningHistoryFlow",
 		func(ctx context.Context, w *Winning) (any, error) {
-			ret.mu.Lock()
-			defer ret.mu.Unlock()
+			rag.mu.Lock()
+			defer rag.mu.Unlock()
 
 			nums := append(w.Numbers, w.Bonus)
 			b, err := json.Marshal(nums)
@@ -95,7 +91,6 @@ func NewLottoRAGAI(
 			if err != nil {
 				return nil, fmt.Errorf("failed to index winning: %w", err)
 			}
-			// log.Printf("indexed: %v", ids)
 
 			return nil, nil
 		},
@@ -104,8 +99,8 @@ func NewLottoRAGAI(
 	pickNumFlow := genkit.DefineFlow(
 		g, "pickLuckyNumsFlow",
 		func(ctx context.Context, cnt int) (Lucky, error) {
-			ret.mu.Lock()
-			defer ret.mu.Unlock()
+			rag.mu.Lock()
+			defer rag.mu.Unlock()
 
 			luckyNums := generateLottoNumbers(7)
 
@@ -114,17 +109,15 @@ func NewLottoRAGAI(
 				return Lucky{}, fmt.Errorf("failed to marshal luckyNums: %w", err)
 			}
 
-			log.Println("lucky numbers", luckyNums)
-			log.Printf("retrieving %d lucky numbers: %s", cnt+50, string(b))
 			retrieves, err := vstore.RetrieveText(ctx, string(b), cnt+50, "issue_no")
 			if err != nil {
 				return Lucky{}, fmt.Errorf("failed to retrieve winning: %w", err)
 			}
 
-			log.Println("retrieved", len(retrieves))
-			for _, r := range retrieves {
-				log.Println(r.Text, r.Metadata)
-			}
+			// log.Println("retrieved", len(retrieves))
+			// for _, r := range retrieves {
+			// 	log.Println(r.Text, r.Metadata)
+			// }
 
 			docs := make([]*ai.Document, len(retrieves))
 			for i, r := range retrieves {
@@ -156,8 +149,8 @@ func NewLottoRAGAI(
 	chatbotFlow := genkit.DefineFlow(
 		g, "chatbotFlow",
 		func(ctx context.Context, msg string) (Cmd, error) {
-			ret.mu.Lock()
-			defer ret.mu.Unlock()
+			rag.mu.Lock()
+			defer rag.mu.Unlock()
 
 			systemPrompt := `너의 이름은 김점지야.
 너의 임무는 사용자의 채팅 메시지를 분석하여 적절한 행동(Action)을 선택하는 것이야.
@@ -195,9 +188,9 @@ func NewLottoRAGAI(
 		},
 	)
 
-	ret.IndexWinningHistoryFlow = indexFlow
-	ret.PickLuckyNumsFlow = pickNumFlow
-	ret.ChatbotFlow = chatbotFlow
+	rag.IndexWinningHistoryFlow = indexFlow
+	rag.PickLuckyNumsFlow = pickNumFlow
+	rag.ChatbotFlow = chatbotFlow
 
-	return ret, nil
+	return rag, nil
 }
